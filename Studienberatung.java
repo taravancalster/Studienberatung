3bc207324c8a0f07bd0105ce1a70b132d7bf8381
Van Calster <Tara.VanCalster@haw-hamburg.de>
2016-10-12 15:22:32 +0200

    added question Hast du Abitur?

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public Studienberatung{
+	
 	public static void main (String [] args){
 		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("Hast du Abitur?");
+		String answer = scanner.next();
 	}
 	
 }
